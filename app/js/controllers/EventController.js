6c08f48d02827d47c143ddc910ce863fddc8c022
Ram Iyer <ramky.iyer@yahoo.com>
2013-12-25 23:39:14 -0800

    adding sessions

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -11,7 +11,30 @@ eventsApp.controller('EventController',
 				city: 'Mountain View',
 				province: 'CA'
 			},
-			imageUrl: '/img/angularjs-logo.png'		
+			imageUrl: '/img/angularjs-logo.png',
+		  sessions: [
+        {
+          name: 'Directives Masterclass',
+          creatorName: 'Bob Smith',
+          duration: '1 hr',
+          level: 'Advanced',
+          abstract: 'In this session you will learn ins and outs of directives.'
+        },
+        {
+          name: 'Scopes for fun and profit',
+          creatorName: 'John Doe',
+          duration: '30 mns',
+          level: 'Introductory',
+          abstract: 'This session will take a closer look at scopes.'
+        },
+        {
+          name: 'Well Behaved Controllers',
+          creatorName: 'Jane Doe',
+          duration: '2 hours',
+          level: 'Intermediate',
+          abstract: 'Controllers are the beginning of everything Angular.'
+        }
+      ],
 		}
 	}
 );
