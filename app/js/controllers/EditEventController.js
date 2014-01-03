a86b95c131c5a4d7085edddf0a5c9b0253b54d17
Ram Iyer <ramky.iyer@yahoo.com>
2014-01-02 21:27:42 -0800

    completed 2 way binding

--- app/js/controllers/EditEventController.js
+++ app/js/controllers/EditEventController.js
@@ -2,5 +2,12 @@
 
 eventsApp.controller('EditEventController',
   function EditEventController($scope){
+    $scope.saveEvent = function(event){
+      window.alert('event ' + event.name + ' saved!');
+    }
+
+    $scope.cancelEdit = function(){
+      window.location = "/event_details.html";
+    }
   }
 );
