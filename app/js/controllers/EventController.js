095fdf59d96b24d8ce656de7f65ed9bc166bb9bf
Ram Iyer <ramky.iyer@yahoo.com>
2014-01-04 22:27:00 -0800

    using built-in http service

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -2,22 +2,24 @@
 
 eventsApp.controller('EventController',
 	function EventController ($scope, eventData) {
-    $scope.snippet = '<span style=\"color: red\">hi there</span>';
-    $scope.boolValue = true;
-    $scope.mystyle = {color: 'red'};
-    $scope.myclass = "blue";
-    $scope.buttonDisabled = true;
-		
-    $scope.sortOrder = 'name';
-    $scope.event = eventData.event;
-    
-    $scope.upVoteSession = function(session){
-      session.upVoteCount++;
-    };
+        $scope.snippet = '<span style=\"color: red\">hi there</span>';
+        $scope.boolValue = true;
+        $scope.mystyle = {color: 'red'};
+        $scope.myclass = "blue";
+        $scope.buttonDisabled = true;
+    		
+        $scope.sortOrder = 'name';
 
-    $scope.downVoteSession = function(session){
-      session.upVoteCount--;
-    };
+        eventData.getEvent(function(event){
+            $scope.event =  event;
+        });
+        
+        $scope.upVoteSession = function(session){
+          session.upVoteCount++;
+        };
 
+        $scope.downVoteSession = function(session){
+          session.upVoteCount--;
+        };
 	}
 );
--- app/js/services/EventData.js
+++ app/js/services/EventData.js
@@ -1,41 +1,14 @@
-eventsApp.factory('eventData', function(){
+eventsApp.factory('eventData', function($http, $log){
   return {
-    event: {
-			name: 'Angular Boot Camp',
-      date: 1388089325967,
-			time: '10:30 AM',
-			location: {
-				address: 'Google Headquarters',
-				city: 'Mountain View',
-				province: 'CA'
-			},
-			imageUrl: '/img/angularjs-logo.png',
-		  sessions: [
-        {
-          name: 'Directives Masterclass',
-          creatorName: 'Bob Smith',
-          duration: 1,
-          level: 'Advanced',
-          abstract: 'In this session you will learn ins and outs of directives.',
-          upVoteCount: 0
-        },
-        {
-          name: 'Scopes for fun and profit',
-          creatorName: 'John Doe',
-          duration: 2,
-          level: 'Introductory',
-          abstract: 'This session will take a closer look at scopes.',
-          upVoteCount: 0
-        },
-        {
-          name: 'Well Behaved Controllers',
-          creatorName: 'Jane Doe',
-          duration: 4,
-          level: 'Intermediate',
-          abstract: 'Controllers are the beginning of everything Angular.',
-          upVoteCount: 0
-        }
-      ],
+    getEvent: function(sucessCallback){
+      $http({method: 'GET', url: '/data/event/1'}).
+        success(function(data, status, headers,config){
+          /*$log.info(data, status, headers, config);*/
+          sucessCallback(data);
+        }).
+        error(function(data, status, headers,config){
+          $log.warn(data, status, headers, config);
+        });
 		}
-  }
+  };
 });
